9dfbecfa8c025bb6b61cce9c4492101fa9908d99
Chris Bond <bondcb@bc.edu>
2022-02-20 14:15:12 -0500

    5.7 complete

--- EU Table View/Nation.swift
+++ EU Table View/Nation.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import UIKit
 
-struct Nation {
+struct Nation: Codable {
     var country: String
     var capital: String
     var usesEuro: Bool
--- EU Table View/ViewController.swift
+++ EU Table View/ViewController.swift
@@ -13,93 +13,93 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var addBarButton: UIBarButtonItem!
     
-    var members = ["Austria",
-                   "Belgium",
-                   "Bulgaria",
-                   "Croatia",
-                   "Cyprus",
-                   "Czechia",
-                   "Denmark",
-                   "Estonia",
-                   "Finland",
-                   "France",
-                   "Germany",
-                   "Greece",
-                   "Hungary",
-                   "Ireland",
-                   "Italy",
-                   "Latvia",
-                   "Lithuania",
-                   "Luxembourg",
-                   "Malta",
-                   "Netherlands",
-                   "Poland",
-                   "Portugal",
-                   "Romania",
-                   "Slovakia",
-                   "Slovenia",
-                   "Spain",
-                   "Sweden",
-                   "United Kingdom"]
-    
-    var capitals = ["Vienna",
-                    "Brussels",
-                    "Sofia",
-                    "Zagreb",
-                    "Nicosia",
-                    "Prague",
-                    "Copenhagen",
-                    "Tallinn",
-                    "Helsinki",
-                    "Paris",
-                    "Berlin",
-                    "Athens",
-                    "Budapest",
-                    "Dublin",
-                    "Rome",
-                    "Riga",
-                    "Vilnius",
-                    "Luxembourg (city)",
-                    "Valetta",
-                    "Amsterdam",
-                    "Warsaw",
-                    "Lisbon",
-                    "Bucharest",
-                    "Bratislava",
-                    "Ljubljana",
-                    "Madrid",
-                    "Stockholm",
-                    "London"]
-    
-    var usesEuro = [true,
-                    true,
-                    false,
-                    false,
-                    true,
-                    false,
-                    false,
-                    true,
-                    true,
-                    true,
-                    true,
-                    true,
-                    false,
-                    true,
-                    true,
-                    true,
-                    true,
-                    true,
-                    true,
-                    true,
-                    false,
-                    true,
-                    false,
-                    true,
-                    true,
-                    true,
-                    false,
-                    false]
-    
+//    var members = ["Austria",
+//                   "Belgium",
+//                   "Bulgaria",
+//                   "Croatia",
+//                   "Cyprus",
+//                   "Czechia",
+//                   "Denmark",
+//                   "Estonia",
+//                   "Finland",
+//                   "France",
+//                   "Germany",
+//                   "Greece",
+//                   "Hungary",
+//                   "Ireland",
+//                   "Italy",
+//                   "Latvia",
+//                   "Lithuania",
+//                   "Luxembourg",
+//                   "Malta",
+//                   "Netherlands",
+//                   "Poland",
+//                   "Portugal",
+//                   "Romania",
+//                   "Slovakia",
+//                   "Slovenia",
+//                   "Spain",
+//                   "Sweden",
+//                   "United Kingdom"]
+//
+//    var capitals = ["Vienna",
+//                    "Brussels",
+//                    "Sofia",
+//                    "Zagreb",
+//                    "Nicosia",
+//                    "Prague",
+//                    "Copenhagen",
+//                    "Tallinn",
+//                    "Helsinki",
+//                    "Paris",
+//                    "Berlin",
+//                    "Athens",
+//                    "Budapest",
+//                    "Dublin",
+//                    "Rome",
+//                    "Riga",
+//                    "Vilnius",
+//                    "Luxembourg (city)",
+//                    "Valetta",
+//                    "Amsterdam",
+//                    "Warsaw",
+//                    "Lisbon",
+//                    "Bucharest",
+//                    "Bratislava",
+//                    "Ljubljana",
+//                    "Madrid",
+//                    "Stockholm",
+//                    "London"]
+//
+//    var usesEuro = [true,
+//                    true,
+//                    false,
+//                    false,
+//                    true,
+//                    false,
+//                    false,
+//                    true,
+//                    true,
+//                    true,
+//                    true,
+//                    true,
+//                    false,
+//                    true,
+//                    true,
+//                    true,
+//                    true,
+//                    true,
+//                    true,
+//                    true,
+//                    false,
+//                    true,
+//                    false,
+//                    true,
+//                    true,
+//                    true,
+//                    false,
+//                    false]
+//
     var nations: [Nation] = []
 
     
@@ -108,9 +108,37 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         
-        for index in 0..<members.count {
-            let newNation = Nation(country: members[index], capital: capitals[index], usesEuro: usesEuro[index])
-            nations.append(newNation)
+        
+//        for index in 0..<members.count {
+//            let newNation = Nation(country: members[index], capital: capitals[index], usesEuro: usesEuro[index])
+//            nations.append(newNation)
+//        }
+        loadData()
+    }
+    
+    func loadData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("nations").appendingPathExtension("json")
+        
+        guard let data = try? Data(contentsOf: documentURL) else {return}
+        let jsonDecoder = JSONDecoder()
+        do {
+            nations = try jsonDecoder.decode(Array<Nation>.self, from: data)
+            tableView.reloadData()
+            } catch {
+                print("ERROR: Could not load Data \(error.localizedDescription)")
+            }
+        }
+    
+    func saveData() {
+        let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let documentURL = directoryURL.appendingPathComponent("nations").appendingPathExtension("json")
+        let jsonEncoder = JSONEncoder()
+        let data = try? jsonEncoder.encode(nations)
+        do {
+            try data?.write(to: documentURL, options: .noFileProtection)
+        } catch {
+            print("ERROR: could not save data")
         }
     }
     
@@ -136,7 +164,7 @@ class ViewController: UIViewController {
             tableView.insertRows(at: [newIndexPath], with: .bottom)
             tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
         }
-
+        saveData()
     }
     @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
         if tableView.isEditing {
@@ -167,12 +195,13 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         if editingStyle == .delete {
             nations.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            saveData()
         }
     }
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let itemToMove = nations[sourceIndexPath.row]
         nations.remove(at: sourceIndexPath.row)
         nations.insert(itemToMove, at: destinationIndexPath.row)
-        
+        saveData()
     }
 }
